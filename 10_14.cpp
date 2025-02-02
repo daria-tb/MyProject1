514674bc774b437c9c0ee7338a29751379d1fb97
daria-tb <dascha.togan1@gmail.com>
2025-02-03 00:03:30 +0200

    10_14.cpp

--- 10_14.cpp
+++ 10_14.cpp
@@ -60,17 +60,17 @@ int main()
     double radius;
     std::cin >> radius;
 
-    const double PI = 3.1415;
+    //const double PI = 3.1415;
     double diameter = radius * 2;
-    double area = PI * radius * radius;
+    double area1 = PI * radius * radius;
     double length = 2 * PI * radius;
 
     std::cout << "----- Circle information--\n";
     std::cout << "Radius = " << radius << std::endl;
     std::cout << "Diameter = " << diameter << std::endl;
-    std::cout << "Area = " << area << std::endl;
+    std::cout << "Area = " << area1 << std::endl;
     std::cout << "Length = " << length << std::endl;
-    
+
     return 0;
 }
 
